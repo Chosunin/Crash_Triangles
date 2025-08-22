137ac317ce64d0135730c3bf49cdd65e5e272263
Chosunin <132040493+Chosunin@users.noreply.github.com>
2025-08-22 18:59:17 +0900

    역삼각형
    이제서야 내가 뭘 쓰고 있는지 알게 되다

---
+++ TriangleEx4.java
@@ -0,0 +1,12 @@
+package Crash_Triangles.src;
+
+public class TriangleEx4 {
+    public static void main(String[] args) {
+        for (int s = 5; s >= 1; s --) {
+            for (int st = 1; st <= s; st++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
